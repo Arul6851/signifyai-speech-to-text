a2c74d434c1b96ae53914dd3cef688a4774e4676
Arul6851 <12304.arul@gmail.com>
2025-04-20 17:05:12 +0530

    bug fix

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
       crypto: require.resolve("crypto-browserify"),
       os: require.resolve("os-browserify/browser"),
       path: require.resolve("path-browserify"),
+      stream: require.resolve("stream-browserify"),
+      vm: require.resolve("vm-browserify"),
+      assert: require.resolve("assert"),
+      util: require.resolve("util/"),
     },
   },
   plugins: [
